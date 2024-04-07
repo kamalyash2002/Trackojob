c308e65f62dfbe15d9da5b1ed226360e21edf3e2
kamalyash2002 <93870977+kamalyash2002@users.noreply.github.com>
2024-04-07 16:41:33 +0530

    Some auth fixes

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,5 +1,4 @@
 const {User} = require("../models/users")
-const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const {v4 : uuidv4} = require('uuid')
 const {setUser , getUser} = require("../service/auth")
