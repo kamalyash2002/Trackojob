b61793dcd4ce4dc780526fba9d36ba2c0879bb4e
kamalyash2002 <93870977+kamalyash2002@users.noreply.github.com>
2024-04-07 19:07:04 +0530

    Auth logics fixes

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -2,13 +2,13 @@ const {User} = require("../models/users")
 const jwt = require("jsonwebtoken")
 const {v4 : uuidv4} = require('uuid')
 const {setUser , getUser} = require("../service/auth")
+const  {createHmac, randomBytes} = require('crypto');
 
 const handleUserSignup = async (req, res) => {
     const { name , email , password } = req.body;
     // add the check for the existing user
     const user = await User.findOne({
         email,
-        password
     });
     if(user){
         return res.json({message: "User already exists"});
@@ -23,15 +23,17 @@ const handleUserSignup = async (req, res) => {
 
 const handleUserSignin = async (req, res) => {
     const { email , password } = req.body;
-    const user = await User.findOne({email, password});
+    const user = await User.findOne({email});
     if(!user){
+        return res.json({message: "Please Signup first"});
+    }
+    const salt = user.salt;
+    const hashedPassword = user.password;
+    const incomingHashedPassword = createHmac('sha256', salt).update(password).digest('hex');
+    if(hashedPassword !== incomingHashedPassword){
         return res.json({message: "Invalid email or password"});
     }
     const token = setUser(user);
-    // setting the cookie
-    // console.log(token);
-    // res.cookie("uid", token);
-
     return res.json({token});
 }
 
